dc4afc38b107767b8256565669a074e23e5d9b5b
NolascoZapata <nolascozapata71@gmail.com>
2023-01-03 19:48:07 -0300

    debug-start

--- index.js
+++ index.js
@@ -5,14 +5,10 @@ const getResultBtn = document.getElementById('getResultBtn')
 for (item of btn) {
   item.addEventListener('click', (e) => {
     btntext = e.target.innerText;
-    result.innerText += btntext;
+    result.value += btntext;
   });
 }
 
 getResultBtn.addEventListener('click', e=>{
-  if ((result.innerText).length>6) {
-  result.innerText=((eval(result.innerText)).toExponential(2))
-  }else{
-  result.innerText=eval(result.innerText)
-  }
+  result.value=eval(result.value)
 })
